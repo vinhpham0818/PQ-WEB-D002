a21d6ed32a56081341b72a28525b72ee0b6dabf2
vinhpham0818 <36657919+vinhpham0818@users.noreply.github.com>
2019-05-06 21:16:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello World!')
+let pg = require('pg')
+let client = new pg.Client(
+   {
+        user: 'postgres',
+        password: '123456',
+        database: 'csv',
+        host: 'localhost',
+        protected: 5432
+   }
+)
+
+client.connect()
+
+client.query('select * from dm_nv limit 1', function(err, result) {
+    console.log(err, result.rows)
+})
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    client.query('select * from dm_nv limit 1', function(err, result) {
+        res.send(JSON.stringify(result.rows))
+        client.end()
+    })
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
